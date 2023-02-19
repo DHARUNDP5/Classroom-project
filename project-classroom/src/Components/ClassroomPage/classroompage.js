a4e7019743deeb861a2e7121e20dc10798ac905c
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-02-19 17:19:19 +0530

    classroom pages

--- project-classroom/src/Components/ClassroomPage/classroompage.js
+++ project-classroom/src/Components/ClassroomPage/classroompage.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const Classroompage=()=> {
+const Classroompage = () => {
   return (
     <div>
-        <div className='h-screen bg-blue-900 flex flex-col space-y-[50px] items-center justify-center text-center text-[25px] text-white'>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Assignment</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Marks</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Database</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Sports</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Class notice</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Teacher profile</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>Exam</button>
-            <button className='hover:bg-red-700 w-full h-[50px]'>logout</button>
+      <div className='h-screen bg-blue-900 flex flex-col space-y-[50px] items-center justify-center text-center text-[25px] text-white'>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Assignment</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Marks</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Database</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Sports</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Class notice</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Teacher profile</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>Exam</button>
+        <button className='hover:bg-red-700 w-full h-[50px]'>logout</button>
 
-        </div>
+      </div>
     </div>
   )
 }
